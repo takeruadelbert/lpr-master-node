65e2bd2b91959682e177e8f65173d34cc75354f6
suryawono <suryawono@gmail.com>
2019-04-01 13:15:51 +0700

    update function only parent

--- src/main/java/com/stn/ester/rest/service/AppService.java
+++ src/main/java/com/stn/ester/rest/service/AppService.java
@@ -1,9 +1,16 @@
 package com.stn.ester.rest.service;
 
 import com.stn.ester.rest.domain.AppDomain;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public abstract class AppService<T extends PagingAndSortingRepository> {
 
@@ -30,7 +37,26 @@ public abstract class AppService<T extends PagingAndSortingRepository> {
         if (old==null){
             return null;
         }
-        object.setId(id);
-        return repository.save(object);
+        copyNonNullProperties(object,old);
+        return repository.save(old);
+    }
+
+
+    //https://stackoverflow.com/questions/27818334/jpa-update-only-specific-fields
+    public static void copyNonNullProperties(Object src, Object target) {
+        BeanUtils.copyProperties(src, target, getNullPropertyNames(src));
+    }
+
+    public static String[] getNullPropertyNames (Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<String>();
+        for(java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) emptyNames.add(pd.getName());
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
     }
 }
