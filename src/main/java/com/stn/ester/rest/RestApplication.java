0df638b95b8cac259444a1930b1998c82700cddd
suryawono <suryawono@gmail.com>
2019-04-05 18:47:37 +0700

    timezone properties

--- src/main/java/com/stn/ester/rest/RestApplication.java
+++ src/main/java/com/stn/ester/rest/RestApplication.java
@@ -1,14 +1,25 @@
 package com.stn.ester.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class RestApplication {
 
+    @Value("${ester.server.timezone}")
+    private String timezone;
+
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
+    }
 
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone(timezone));
     }
 
 }
