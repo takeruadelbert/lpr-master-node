182f62c9fd5189587d5b4209f4d9b4604cc8a6f2
suryawono <suryawono@gmail.com>
2019-05-06 15:14:17 +0700

    appservice, preupdate disable 2 level or more check

--- src/main/java/com/stn/ester/rest/service/AppService.java
+++ src/main/java/com/stn/ester/rest/service/AppService.java
@@ -19,64 +19,66 @@ import java.util.Optional;
 
 public abstract class AppService {
 
-    protected HashMap<String,PagingAndSortingRepository> repositories;
+    protected HashMap<String, PagingAndSortingRepository> repositories;
     protected String baseRepoName;
 
     @PersistenceContext
     protected EntityManager entityManager;
 
-    public AppService(String baseRepoName){
-        repositories=new HashMap();
-        this.baseRepoName=baseRepoName;
+    public AppService(String baseRepoName) {
+        repositories = new HashMap();
+        this.baseRepoName = baseRepoName;
     }
 
-    public Page<Object> index(Integer page,Integer size){
-        return repositories.get(baseRepoName).findAll(PageRequest.of(page,size));
+    public Page<Object> index(Integer page, Integer size) {
+        return repositories.get(baseRepoName).findAll(PageRequest.of(page, size));
     }
 
     @Transactional
-    public Object create(AppDomain o){
-        Object saved=repositories.get(baseRepoName).save(o);
+    public Object create(AppDomain o) {
+        Object saved = repositories.get(baseRepoName).save(o);
         entityManager.refresh(saved);
         return saved;
     }
 
-    public Object get(Long id){
+    public Object get(Long id) {
         if (repositories.get(baseRepoName).existsById(id)) {
             return repositories.get(
                     baseRepoName).findById(id).get();
-        }else{
+        } else {
             throw new ResourceNotFoundException();
         }
     }
 
-    public Object update (Long id,AppDomain object){
-        AppDomain old=(AppDomain)repositories.get(baseRepoName).findById(id).get();
-        if (old==null){
+    public Object update(Long id, AppDomain object) {
+        AppDomain old = (AppDomain) repositories.get(baseRepoName).findById(id).get();
+        if (old == null) {
             throw new ResourceNotFoundException();
         }
-        preUpdate(object,old);
+        preUpdate(object, old);
         return repositories.get(baseRepoName).save(old);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         repositories.get(baseRepoName).deleteById(id);
     }
 
-    private void preUpdate(AppDomain src,AppDomain target){
+    private void preUpdate(AppDomain src, AppDomain target) {
         if (target.isPreparedForUpdate)
             return;
         BeanUtils.copyProperties(src, target, UpdaterHelper.getNullPropertyNames(src));
         target.setPreparedForUpdate(true);
         final BeanWrapper bw = new BeanWrapperImpl(target);
         java.beans.PropertyDescriptor[] pds = bw.getPropertyDescriptors();
-        for(java.beans.PropertyDescriptor pd : pds) {
+        for (java.beans.PropertyDescriptor pd : pds) {
             Object srcValue = bw.getPropertyValue(pd.getName());
-            if (srcValue != null && AppDomain.class.isAssignableFrom(srcValue.getClass()) ){
-                AppDomain toCompare=(AppDomain) srcValue;
-                AppDomain toSave = (AppDomain) repositories.get(toCompare.underscoreName()).findById(toCompare.getId()).get();
-                preUpdate(toCompare, toSave);
-                bw.setPropertyValue(pd.getName(), toSave);
+            if (srcValue != null && AppDomain.class.isAssignableFrom(srcValue.getClass())) {
+                AppDomain toCompare = (AppDomain) srcValue;
+                if (repositories.containsKey(toCompare.underscoreName())) {
+                    AppDomain toSave = (AppDomain) repositories.get(toCompare.underscoreName()).findById(toCompare.getId()).get();
+                    preUpdate(toCompare, toSave);
+                    bw.setPropertyValue(pd.getName(), toSave);
+                }
             }
         }
     }
