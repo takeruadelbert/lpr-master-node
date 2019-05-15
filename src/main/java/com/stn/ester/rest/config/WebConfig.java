d9843e858915aaca26f421eaf0371d1a37f0aa4b
suryawono <suryawono@gmail.com>
2019-05-15 15:36:08 +0700

    enable cors

---
+++ src/main/java/com/stn/ester/rest/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.stn.ester.rest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
