98122a060286758d3a13982fb2f59e8ab9bb1eb3
unknown <fajarahmat05@gmail.com>
2019-06-13 14:17:04 +0700

    create relation ProfilePicture to User

--- src/main/java/com/stn/ester/rest/domain/ProfilePicture.java
+++ src/main/java/com/stn/ester/rest/domain/ProfilePicture.java
@@ -1,9 +1,13 @@
 package com.stn.ester.rest.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Data
 @Entity
@@ -12,6 +16,11 @@ public class ProfilePicture extends AppDomain {
 
     @Column(columnDefinition = "TEXT")
     public String avatar;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
 
     @Override
     public String underscoreName() { return null; }
