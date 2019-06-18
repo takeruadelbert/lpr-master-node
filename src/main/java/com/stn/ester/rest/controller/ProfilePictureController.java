8c06cd72ccb57f0cff3f1d355c04dc6e1893dd45
unknown <fajarahmat05@gmail.com>
2019-06-18 17:15:22 +0700

    remove not uses code

--- src/main/java/com/stn/ester/rest/controller/ProfilePictureController.java
+++ src/main/java/com/stn/ester/rest/controller/ProfilePictureController.java
@@ -4,19 +4,14 @@ import com.stn.ester.rest.dao.jpa.ProfilePictureRespository;
 import com.stn.ester.rest.domain.File;
 import com.stn.ester.rest.domain.ProfilePicture;
 import com.stn.ester.rest.service.ProfilePictureService;
-import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.activation.FileTypeMap;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-import java.beans.XMLDecoder;
 import java.io.*;
 import java.nio.file.Files;
 import java.util.Base64;
@@ -29,9 +24,6 @@ public class ProfilePictureController extends AppController<ProfilePictureServic
     private ProfilePictureService profilePictureService;
 
     @Autowired
-    private ProfilePictureRespository profilePictureRespository;
-
-    @Autowired
     public ProfilePictureController(ProfilePictureService profilePictureService){
         super(profilePictureService);
     }
@@ -40,9 +32,8 @@ public class ProfilePictureController extends AppController<ProfilePictureServic
     public Object convertImage(@RequestParam("file") MultipartFile file) throws IOException {
 
         //serv image to resource folder
-        String path2 = System.getProperty("user.dir") + "\\src\\main\\resources\\files\\images\\" + file.getOriginalFilename(); //set path directory
-
-        FileOutputStream fout = new FileOutputStream(path2);
+        String path = System.getProperty("user.dir") + "\\src\\main\\resources\\files\\images\\" + file.getOriginalFilename(); //set path directory
+        FileOutputStream fout = new FileOutputStream(path);
         fout.write(file.getBytes());
         fout.close();
         //end
@@ -62,7 +53,6 @@ public class ProfilePictureController extends AppController<ProfilePictureServic
 
             ProfilePicture profilePicture = new ProfilePicture();
             profilePicture.avatar = base64Image;
-
             profilePictureService.save(profilePicture);
 
         } catch (FileNotFoundException e) {
