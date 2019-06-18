40fa18d36d5c3977bbde35c110c3f14eea7d2eb1
Lukas Fam <takeruadelbert@gmail.com>
2019-06-18 11:02:39 +0700

    update CRUD of Department (SubDepartment included)

---
+++ src/main/java/com/stn/ester/rest/domain/Department.java
@@ -0,0 +1,57 @@
+package com.stn.ester.rest.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@Entity
+public class Department extends AppDomain {
+    public static String unique_name = "department";
+
+    @NotBlank(message = "Name is mandatory.")
+    @Column(nullable = false, unique = true)
+    private String name;
+    private String label;
+
+    @Transient
+    @EqualsAndHashCode.Exclude
+    private Set<Department> subDepartment = new HashSet<Department>();
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "parent_department_id", insertable = false, updatable = false)
+    @JsonIgnore
+    @EqualsAndHashCode.Exclude
+    private Department parentDepartment;
+
+    @JsonProperty("parentDepartmentId")
+    @Column(name = "parent_department_id")
+    private Long parentDepartmentId;
+
+    @JsonSetter("parentDepartmentId")
+    public void setParentDepartmentId(long parentDepartmentId) {
+        if (parentDepartmentId != 0)
+            this.parentDepartmentId = parentDepartmentId;
+    }
+
+    public Long getParentDepartmentId() {
+        return parentDepartmentId;
+    }
+
+    public void mergeSubDepartment(List<Department> subDepartment) {
+        this.subDepartment.addAll(subDepartment);
+    }
+
+    @Override
+    public String underscoreName() {
+        return unique_name;
+    }
+}
---
+++ src/main/java/com/stn/ester/rest/service/DepartmentService.java
@@ -0,0 +1,59 @@
+package com.stn.ester.rest.service;
+
+import com.stn.ester.rest.dao.jpa.DepartmentRepositoty;
+import com.stn.ester.rest.domain.Department;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class DepartmentService extends AppService {
+    private DepartmentRepositoty departmentRepositoty;
+
+    @Autowired
+    public DepartmentService(DepartmentRepositoty departmentRepositoty) {
+        super(Department.unique_name);
+        super.repositories.put(Department.unique_name, departmentRepositoty);
+        this.departmentRepositoty = departmentRepositoty;
+    }
+
+    @Override
+    public Object get(Long id) {
+        if (repositories.get(baseRepoName).existsById(id)) {
+            Object o = this.departmentRepositoty.findById(id).get();
+            List<Department> subDepartments = this.findSubDepartment(id);
+            ((Department) o).mergeSubDepartment(subDepartments);
+            return o;
+        } else {
+            throw new ResourceNotFoundException();
+        }
+    }
+
+    private List<Department> findSubDepartment(Long parentId) {
+        return this.findSubDepartment(parentId, null);
+    }
+
+    private List<Department> findSubDepartment(Long parentId, Set<Long> departmentIds) {
+        List<Department> subDepartments = this.departmentRepositoty.findAllByParentDepartmentId(parentId);
+        if (!subDepartments.isEmpty()) {
+            Iterator<Department> subDepartmentIterator = subDepartments.iterator();
+            while (subDepartmentIterator.hasNext()) {
+                Department department = subDepartmentIterator.next();
+                if (departmentIds != null && !departmentIds.contains(department.getId())) {
+                    subDepartmentIterator.remove();
+                    continue;
+                }
+                List<Department> subDepartmentIt = this.findSubDepartment(department.getId(), departmentIds);
+                int n = subDepartments.indexOf(department);
+                Department mergedDepartment = subDepartments.get(n);
+                mergedDepartment.mergeSubDepartment(subDepartmentIt);
+                subDepartments.set(n, mergedDepartment);
+            }
+        }
+        return subDepartments;
+    }
+}
