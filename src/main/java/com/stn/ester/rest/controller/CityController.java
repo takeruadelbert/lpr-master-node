2546b2f43b2420d123097e56ca1f71e9bb7b0f5a
Lukas Fam <takeruadelbert@gmail.com>
2019-06-19 15:25:40 +0700

    added function to fetch list cities by its state ID

--- src/main/java/com/stn/ester/rest/controller/CityController.java
+++ src/main/java/com/stn/ester/rest/controller/CityController.java
@@ -2,6 +2,7 @@ package com.stn.ester.rest.controller;
 
 import com.stn.ester.rest.domain.City;
 import com.stn.ester.rest.service.CityService;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class CityController extends AppController<CityService, City> {
     public CityController(CityService cityService) {
         super(cityService);
+    }
+
+    @RequestMapping(value = "/list-by-state/{state_id}", method = RequestMethod.GET)
+    public Object getCityListByState(@PathVariable long state_id) {
+        return service.getCityListByState(state_id);
     }
 }
--- src/main/java/com/stn/ester/rest/service/CityService.java
+++ src/main/java/com/stn/ester/rest/service/CityService.java
@@ -5,6 +5,9 @@ import com.stn.ester.rest.domain.City;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+
 @Service
 public class CityService extends AppService {
     private CityRepository cityRepository;
@@ -14,5 +17,16 @@ public class CityService extends AppService {
         super(City.unique_name);
         super.repositories.put(City.unique_name, cityRepository);
         this.cityRepository = cityRepository;
+    }
+
+    public Object getCityListByState(long state_id) {
+        HashMap<Long,String> result = new HashMap<>();
+        List<City> cities = this.cityRepository.findAllByStateId(state_id);
+        if(!cities.isEmpty()) {
+            for(City city : cities) {
+                result.put(city.getId(), city.getName());
+            }
+        }
+        return result;
     }
 }
