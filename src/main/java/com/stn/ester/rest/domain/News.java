0479cfc61de72f4c461a193853930788007a8c7e
Lukas Fam <takeruadelbert@gmail.com>
2019-06-26 12:45:24 +0700

    added News feature to show/hide it by set its status or period of its date.
    also added a new Helper (DateTimeHelper).

--- src/main/java/com/stn/ester/rest/domain/News.java
+++ src/main/java/com/stn/ester/rest/domain/News.java
@@ -3,9 +3,11 @@ package com.stn.ester.rest.domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.Date;
 
 @Data
 @Entity
@@ -23,16 +25,49 @@ public class News extends AppDomain {
     private String thumbnail_path;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "user_id", insertable = false, updatable = false)
-    private User user;
+    @JoinColumn(name = "author_id", insertable = false, updatable = false)
+    private User author;
 
-    @Column(name = "user_id", nullable = false)
-    private Long userId;
+    @Column(name = "author_id", nullable = false)
+    private Long authorId;
 
-    @JsonSetter("userId")
-    public void setUserId(long userId) {
-        if (userId != 0)
-            this.userId = userId;
+    @JsonSetter("authorId")
+    public void setAuthorId(long authorId) {
+        if (authorId != 0)
+            this.authorId = authorId;
+    }
+
+    @Column(columnDefinition = "Date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date startDate;
+
+    @Column(columnDefinition = "Date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date expiredDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "news_status_id", insertable = false, updatable = false)
+    private NewsStatus newsStatus;
+
+    @Column(name = "news_status_id", nullable = false)
+    private Long newsStatusId;
+
+    @JsonSetter("newsStatusId")
+    public void setNewsStatusId(long newsStatusId) {
+        if (newsStatusId != 0)
+            this.newsStatusId = newsStatusId;
+    }
+
+    public Date getStartDate() {
+        return this.startDate;
+    }
+
+    public Date getExpiredDate() {
+        return this.expiredDate;
+    }
+
+    public long getNewsStatusId() {
+        return this.newsStatusId;
     }
 
     public String underscoreName() {
--- src/main/java/com/stn/ester/rest/service/NewsService.java
+++ src/main/java/com/stn/ester/rest/service/NewsService.java
@@ -3,11 +3,18 @@ package com.stn.ester.rest.service;
 import com.stn.ester.rest.dao.jpa.NewsRepository;
 import com.stn.ester.rest.domain.AppDomain;
 import com.stn.ester.rest.domain.News;
+import com.stn.ester.rest.helper.DateTimeHelper;
 import com.stn.ester.rest.helper.SessionHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class NewsService extends AppService {
@@ -23,15 +30,42 @@ public class NewsService extends AppService {
     @Transactional
     @Override
     public Object create(AppDomain o) {
-        long user_id = SessionHelper.getUserID();
-        ((News) o).setUserId(user_id);
+        long author_id = SessionHelper.getUserID();
+        ((News) o).setAuthorId(author_id);
+        ((News) o).setNewsStatusId(1); // default option is showed.
         return super.create(o);
     }
 
     @Override
     public Object update(Long id, AppDomain o) {
-        long user_id = SessionHelper.getUserID();
-        ((News) o).setUserId(user_id);
+        long author_id = SessionHelper.getUserID();
+        ((News) o).setAuthorId(author_id);
         return super.update(id, o);
+    }
+
+    public Object getAllValidNews() throws Exception {
+        List<News> validNews = new ArrayList<>();
+        Iterable<News> allNews = this.newsRepository.findAll();
+
+        // get current date
+        Date today = DateTimeHelper.getCurrentDate();
+
+        for(News news : allNews) {
+            // check if period date of news exists
+            if(news.getStartDate() != null && news.getExpiredDate() != null) {
+                // check if news still valid within period of date
+                Date startDate = news.getStartDate();
+                Date expiredDate = news.getExpiredDate();
+                if(!startDate.after(today) && !expiredDate.before(today)) {
+                    validNews.add(news);
+                }
+            } else {
+                // otherwise just get all news status id 1 -> showing
+                if(news.getNewsStatusId() == 1) {
+                    validNews.add(news);
+                }
+            }
+        }
+        return validNews;
     }
 }
