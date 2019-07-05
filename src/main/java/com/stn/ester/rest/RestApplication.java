eb253e569a4e69406274c1b636dfc83ca3ec896e
Lukas Fam <takeruadelbert@gmail.com>
2019-07-05 11:52:36 +0700

    added default profile picture in asset default at the beginning running program

--- src/main/java/com/stn/ester/rest/RestApplication.java
+++ src/main/java/com/stn/ester/rest/RestApplication.java
@@ -1,5 +1,8 @@
 package com.stn.ester.rest;
 
+import com.stn.ester.rest.dao.jpa.AssetFileRepository;
+import com.stn.ester.rest.domain.AssetFile;
+import com.stn.ester.rest.helper.GlobalFunctionHelper;
 import org.omg.CORBA.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +12,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
+import java.nio.file.Path;
 import java.util.TimeZone;
 
 @SpringBootApplication
@@ -16,18 +21,45 @@ public class RestApplication extends SpringBootServletInitializer {
 
     @Value("${ester.server.timezone}")
     private String timezone;
+    private String parentDirectory = new File(System.getProperty("user.dir")).getParent();
+    @Value("${ester.asset.root}")
+    private String assetPath;
+    @Value("${ester.asset.default}")
+    private String assetDefault;
+    private static final String DS = File.separator;
+
+    @Autowired
+    private AssetFileRepository assetFileRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
     }
 
     @PostConstruct
-    public void init(){
+    public void init() {
         TimeZone.setDefault(TimeZone.getTimeZone(timezone));
+        this.addDefaultProfilePicture();
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(RestApplication.class);
+    }
+
+    private void addDefaultProfilePicture() {
+        try {
+            String assetDefaultDir = this.parentDirectory + DS + this.assetPath + DS + this.assetDefault;
+
+            // create asset default dir if it doesn't exist
+            GlobalFunctionHelper.autoCreateDir(assetDefaultDir);
+
+            // add default profile picture to Asset File table
+            String filename = "default-pp.png";
+            String defaultProfilePictPath = DS + this.assetPath + DS + this.assetDefault + DS + filename;
+            AssetFile defaultPP = new AssetFile(defaultProfilePictPath, GlobalFunctionHelper.getNameFile(filename), GlobalFunctionHelper.getExtensionFile(filename));
+            this.assetFileRepository.save(defaultPP);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
