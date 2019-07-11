bd075f7079262aa804132b034731f3c7c6eefdc5
Lukas Fam <takeruadelbert@gmail.com>
2019-07-11 14:46:49 +0700

    added enumerate data Gender

--- src/main/java/com/stn/ester/rest/domain/Biodata.java
+++ src/main/java/com/stn/ester/rest/domain/Biodata.java
@@ -3,17 +3,17 @@ package com.stn.ester.rest.domain;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
+import com.stn.ester.rest.domain.enumerate.Gender;
 import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
-public class Biodata extends AppDomain{
+public class Biodata extends AppDomain {
 
-    public static final String unique_name="biodata";
+    public static final String unique_name = "biodata";
 
     @NotBlank(message = "First Name is mandatory.")
     @Column(nullable = false)
@@ -36,19 +36,8 @@ public class Biodata extends AppDomain{
     @JsonBackReference
     private User user;
 
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "gender_id", insertable = false, updatable = false)
+    @Enumerated(EnumType.STRING)
     private Gender gender;
-
-    @JsonProperty("genderId")
-    @Column(name = "gender_id")
-    private Long genderId;
-
-    @JsonSetter("genderId")
-    public void setGenderId(long genderId) {
-        if (genderId != 0)
-            this.genderId = genderId;
-    }
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "identity_type_id", insertable = false, updatable = false)
@@ -63,6 +52,7 @@ public class Biodata extends AppDomain{
         if (identityTypeId != 0)
             this.identityTypeId = identityTypeId;
     }
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "country_id", insertable = false, updatable = false)
     private Country country;
@@ -73,7 +63,7 @@ public class Biodata extends AppDomain{
 
     @JsonSetter("countryId")
     public void setCountryId(long countryId) {
-        if(countryId != 0)
+        if (countryId != 0)
             this.countryId = countryId;
     }
 
@@ -86,7 +76,7 @@ public class Biodata extends AppDomain{
     private Long stateId;
 
     public void setStateId(long stateId) {
-        if(stateId != 0)
+        if (stateId != 0)
             this.stateId = stateId;
     }
 
@@ -99,7 +89,7 @@ public class Biodata extends AppDomain{
     private Long cityId;
 
     public void setCityId(long cityId) {
-        if(cityId != 0)
+        if (cityId != 0)
             this.cityId = cityId;
     }
 
