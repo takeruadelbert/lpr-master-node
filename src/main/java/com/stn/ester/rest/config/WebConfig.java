d1981cda7c07864c7d9639d2171444de48981817
Surya Wono <suryawono@gmail.com>
2019-08-01 16:57:41 +0700

    enable cors

--- src/main/java/com/stn/ester/rest/config/WebConfig.java
+++ src/main/java/com/stn/ester/rest/config/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
+
     }
 }
