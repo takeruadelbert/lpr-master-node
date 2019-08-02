a678908c5aef68644208225acc39ab8a5118a72b
Lukas Fam <takeruadelbert@gmail.com>
2019-08-02 11:47:42 +0700

    fix bug always add data system profile although it's already exist.

--- src/main/java/com/stn/ester/rest/dao/jpa/SystemProfileRepository.java
+++ src/main/java/com/stn/ester/rest/dao/jpa/SystemProfileRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SystemProfileRepository extends AppRepository<SystemProfile, IdList> {
-
+    SystemProfile findFirstByIdIsNotNull();
 }
