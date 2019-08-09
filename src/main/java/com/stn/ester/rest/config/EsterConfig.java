c8f25d86af90f805ef76cd69c942e5a346396d54
Lukas Fam <takeruadelbert@gmail.com>
2019-08-09 15:45:13 +0700

    fix bug upload file : file couldn't be uploaded when deployed in .war file

--- src/main/java/com/stn/ester/rest/config/EsterConfig.java
+++ src/main/java/com/stn/ester/rest/config/EsterConfig.java
@@ -1,20 +1,20 @@
 package com.stn.ester.rest.config;
 
 import com.stn.ester.rest.filter.RequestLoggingFilter;
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 
 @Component
 public class EsterConfig {
-
+    @Value("${ester.upload.max-file-size}")
+    private Long maxUploadFileSize;
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
@@ -30,5 +30,12 @@ public class EsterConfig {
         FilterRegistrationBean<RequestLoggingFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new RequestLoggingFilter());
         return registrationBean;
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(maxUploadFileSize);
+        return multipartResolver;
     }
 }
