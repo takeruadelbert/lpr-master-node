6e59f8f8199c4c4d77603fb8d225bedaec1b818e
unknown <fajarahmat05@gmail.com>
2019-09-05 14:25:04 +0700

    update code extract all value to instant variable in system profile entity

--- src/main/java/com/stn/ester/rest/domain/SystemProfile.java
+++ src/main/java/com/stn/ester/rest/domain/SystemProfile.java
@@ -15,36 +15,47 @@ import java.util.Set;
 @Data
 @Entity
 public class SystemProfile extends AppDomain {
-    public static final String COLUMN_LOGO_ID = "logo_id";
+    private static final String COLUMN_LOGO_ID = "logo_id";
+    private static final String COLUMN_CITY_ID = "city_id";
+    private static final String COLUMN_COUNTRY_ID = "country_id";
+    private static final String COLUMN_IMAGE_BACKGROUND_ID = "image_background_id";
+    private static final String MAPPED_BY_SYSTEM_PROFILE = "systemProfile";
+    private static final String JSON_PROPERTY_ASSET_FILE_ID = "assetFileId";
+    private static final String JSON_PROPERTY_IMAGE_BACKGROUND_ID = "imageBackgroundId";
+    private static final String COLUMN_DEFINITION_TEXT = "TEXT";
+    private static final String VALIDATOR_EMAIL_MESSAGE = "Invalid Email Format.";
+    private static final String VALIDATOR_URL_MESSAGE = "Invalid URL Website.";
+    private static final String VALIDATOR_URL_REGEXP = "^(http|https).*";
+
     public static final String unique_name = "system_profile";
 
     @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = COLUMN_LOGO_ID, insertable = false, updatable = false)
     private AssetFile assetFile;
 
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "systemProfile" ,cascade = CascadeType.ALL)
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = MAPPED_BY_SYSTEM_PROFILE ,cascade = CascadeType.ALL)
     @JsonManagedReference
     @EqualsAndHashCode.Exclude
     private Set<SocialMedia> socialMedia =  new HashSet<>();
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "city_id", insertable = false, updatable = false)
+    @JoinColumn(name = COLUMN_CITY_ID, insertable = false, updatable = false)
     private City city;
 
     @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "country_id", insertable = false, updatable = false)
+    @JoinColumn(name = COLUMN_COUNTRY_ID, insertable = false, updatable = false)
     private Country country;
 
     @OneToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "image_background_id", insertable = false, updatable = false)
+    @JoinColumn(name = COLUMN_IMAGE_BACKGROUND_ID, insertable = false, updatable = false)
     private AssetFile imageBackground;
 
-    @JsonProperty("assetFileId")
-    @Column(name = "logo_id")
+    @JsonProperty(JSON_PROPERTY_ASSET_FILE_ID)
+    @Column(name = COLUMN_LOGO_ID)
     private Long assetFileId;
 
-    @JsonProperty("imageBackgroundId")
-    @Column(name = "image_background_id")
+    @JsonProperty(JSON_PROPERTY_IMAGE_BACKGROUND_ID)
+    @Column(name = COLUMN_IMAGE_BACKGROUND_ID)
     private Long imageBackgroundId;
 
     public void setAssetFileId(Long assetFileId) {
@@ -60,11 +71,11 @@ public class SystemProfile extends AppDomain {
     private String telephone;
     private String name;
     private String shortname;
-    @Column(columnDefinition = "TEXT")
+    @Column(columnDefinition = COLUMN_DEFINITION_TEXT)
     private String header;
-    @Email(message = "Invalid Email Format.")
+    @Email(message = VALIDATOR_EMAIL_MESSAGE)
     private String email;
-    @URL(regexp = "^(http|https).*", message = "Invalid URL Website")
+    @URL(regexp = VALIDATOR_URL_REGEXP, message = VALIDATOR_URL_MESSAGE)
     private String website;
     private String appName;
     private String appShortName;
