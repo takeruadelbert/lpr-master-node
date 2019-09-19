a03509bbf7009fe411550a581e8d92f4c6f932e6
Surya Wono <suryawono@gmail.com>
2019-09-19 15:25:09 +0700

    minor

--- src/main/java/com/stn/ester/services/base/RepositoryAware.java
+++ src/main/java/com/stn/ester/services/base/RepositoryAware.java
@@ -1,9 +1,8 @@
 package com.stn.ester.services.base;
 
-import com.stn.ester.entities.base.BaseEntity;
 import com.stn.ester.repositories.jpa.base.BaseRepository;
 
-public interface RepositoryAware<U extends BaseEntity, T extends BaseRepository> {
+public interface RepositoryAware<T extends BaseRepository> {
 
     T getRepository();
 }
