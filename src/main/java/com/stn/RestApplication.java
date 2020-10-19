167e0069ae8cb6cd7d32574654b8b4bcb0705b28
Surya Wono <suryawono@gmail.com>
2020-10-19 08:34:32 +0000

    Merged in ES-451-clean-runner-class (pull request #183)
    cleaning main class

--- src/main/java/com/stn/RestApplication.java
+++ src/main/java/com/stn/RestApplication.java
@@ -1,109 +1,24 @@
 package com.stn;
 
-import com.stn.ester.entities.Biodata;
-import com.stn.ester.entities.Role;
-import com.stn.ester.entities.User;
-import com.stn.ester.services.crud.*;
-import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
-
-import static com.stn.ester.core.security.SecurityConstants.ROLE_SUPERADMIN;
 
 @SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 @EnableRabbit
 @EnableAsync
 public class RestApplication extends SpringBootServletInitializer {
 
-    @Value("${ester.server.timezone}")
-    private String timezone;
-
-    @Value("${queue.access-log.name}")
-    private String queueName;
-
-    @Autowired
-    private AssetFileService assetFileService;
-
-    @Autowired
-    private RoleService roleService;
-
-    @Autowired
-    private UserService userService;
-
-
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
-    }
-
-    @PostConstruct
-    public void init() {
-        TimeZone.setDefault(TimeZone.getTimeZone(timezone));
-        this.addDefaultProfilePicture();
-        this.addSuperAdmin();
-        this.addDefaultSystemProfile();
     }
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(RestApplication.class);
-    }
-
-    private void addDefaultProfilePicture() {
-        this.assetFileService.addDefaultProfilePicture();
-    }
-
-    private void addDefaultSystemProfile() {
-        this.assetFileService.addDefaultSystemProfileIfDoesntExist();
-    }
-
-    public void addSuperAdmin() {
-        Role role = new Role();
-        role.setName(ROLE_SUPERADMIN);
-        role.setLabel("Super Admin");
-        role = (Role) roleService.createIfNameNotExist(role, ROLE_SUPERADMIN);
-        User user = new User();
-        String username = "admin";
-        user.setUsername(username);
-        user.setPassword("adminstn");
-        user.setEmail("info@suryateknologi.co.id");
-        user.addRole(role);
-        Biodata biodata = new Biodata();
-        biodata.setFirstName("STN");
-        biodata.setLastName("Ester");
-        biodata.setUser(user);
-        user.setBiodata(biodata);
-        userService.createIfUsernameNotExist(user, username);
-    }
-
-    @Bean
-    public Queue queue() {
-        return new Queue(queueName, true);
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
-        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
-        return rabbitTemplate;
-    }
-
-    @Bean
-    public MessageConverter producerJackson2MessageConverter() {
-        return new Jackson2JsonMessageConverter();
     }
 }
