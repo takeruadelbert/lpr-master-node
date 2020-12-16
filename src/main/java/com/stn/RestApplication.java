d1359cbbb2cc5d09a2aa35fa9b085decc1cd9c45
Lukas Fam <takeruadelbert@gmail.com>
2020-12-16 15:45:15 +0700

    added eureka client service

--- src/main/java/com/stn/RestApplication.java
+++ src/main/java/com/stn/RestApplication.java
@@ -7,12 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(exclude = RepositoryRestMvcAutoConfiguration.class)
 @EnableRabbit
 @EnableAsync
 @Slf4j
+@EnableEurekaClient
 public class RestApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
